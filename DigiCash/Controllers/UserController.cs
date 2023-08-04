60a74a3874a6084ede859338e22c2080bd1edfdf
Serdar YILDIZ <75043412+serdaryyildiz@users.noreply.github.com>
2023-08-04 09:20:38 +0300

    Update UserController.cs

---
+++ DigiCash/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using DigiCash.Models;
+
+namespace DigiCash.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class UserController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public UserController(UserManager<IdentityUser> userManager , SignInManager<IdentityUser> signInManager)
+        {
+            userManager = _userManager;
+            signInManager = _signInManager;
+        }
+
+        public async Task<IActionResult> Register([FromForm] Login log)
+        {
+            return Ok();
+        }
+
+        public async Task<IActionResult> Login([FromForm] Login log)
+        {
+            return Ok();
+        }
+    }
+}

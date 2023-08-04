429b2fff1f8e2b850c3af1757206a71d8a2b3177
Serdar YILDIZ <75043412+serdaryyildiz@users.noreply.github.com>
2023-08-04 14:32:19 +0300

    user update

--- DigiCash/Models/User.cs
+++ DigiCash/Models/User.cs
@@ -1,13 +1,22 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 namespace DigiCash.Models
 {
     public class User 
     {
         Wallet wallet;
-        public string? id { get; set; } = string.Empty;
+        private string? _tc;
+        [Required(ErrorMessage = "Id'nizi girmek zorunludur.")]
+
+        public string? password { get; set; }
+        [MinLength(6, ErrorMessage = "Şifreniz en az 6 karakter içermelidir.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z]).+$", ErrorMessage = "Şifrenizde en az bir büyük ve bir küçük karakter kullanmanız gerekmektedir.")]
+
+
+        public string id { get; set; }
         public string firstName { get; set; } = string.Empty;
         public string lastName { get; set; } = string.Empty;
-        public string fullName => $"{firstName} {lastName.ToUpper()}";
+        public string fullName => $"{firstName} {lastName?.ToUpper()}";
 
         public User()
         {

9a67493ea62656b319541ed14b3e93cf196de6ea
Serdar YILDIZ <yildiz.serdaryy@gmail.com>
2023-08-08 01:36:02 +0300

    update services

---
+++ DigiCash/Services/TransactionService.cs
@@ -0,0 +1,34 @@
+﻿using DigiCash.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using MongoDB.Driver;
+using System.Runtime.CompilerServices;
+using System.Transactions;
+
+namespace DigiCash.Services
+{
+    public class TransactionService
+    {
+        private readonly IMongoCollection<Transaction> _transactions;
+
+        public TransactionService(String connectionString)
+        {
+            
+        }
+
+        public void DatabaseConnection()
+        {
+
+        }
+
+        public async void addHistory(ProcessHistory processHistory)
+        {
+            
+        }
+
+        public async void showHistory(Wallet walletId)
+        {
+
+        }
+    } 
+}

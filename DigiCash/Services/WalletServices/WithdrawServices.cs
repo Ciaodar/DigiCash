f10ff30309a5690e0e7ce79e9a151078559b7863
Metehan <mehmetmetehan.cavdar@agu.edu.tr>
2023-08-10 14:35:27 +0300

    withdraw service implemented

--- DigiCash/Services/WalletServices/WithdrawServices.cs
+++ DigiCash/Services/WalletServices/WithdrawServices.cs
@@ -3,8 +3,20 @@ namespace DigiCash.Services.WalletServices
 {
     public class WithdrawServices
     {
-        public WithdrawServices()
+        AmountServices _amountServices;
+        PostgreSqlServices _postgreSqlServices;
+        public WithdrawServices(AmountServices amountServices, PostgreSqlServices postgreSqlServices)
         {
+            _amountServices = amountServices;
+            _postgreSqlServices = postgreSqlServices;
+        }
+        public async Task<bool> withdraw(string walletId, double amount)
+        {
+            if (await _amountServices.CheckWithdrawAmount(walletId, amount))
+            {
+                return true;
+            }
+            return false;
         }
     }
 }

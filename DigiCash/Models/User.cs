721d1382f640705c7f7b3d6ed332ea666bba356d
Serdar YILDIZ <yildiz.serdaryy@gmail.com>
2023-08-16 11:58:23 +0300

    userservice

--- DigiCash/Models/User.cs
+++ DigiCash/Models/User.cs
@@ -4,16 +4,15 @@ namespace DigiCash.Models
 {
     public class User 
     {
-        private string? _tc;
-        [Required(ErrorMessage = "Id'nizi girmek zorunludur.")]
-
+        public string? tc;
+        [Required(ErrorMessage = "TC'nizi girmek zorunludur.")]
         public string firstName { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Lütfen bir isim giriniz.")]
         public string lastName { get; set; } = string.Empty;
-
+        [Required(ErrorMessage = "Lütfen soyadınızı giriniz.")]
         public string? password { get; set; }
         [MinLength(6, ErrorMessage = "Şifreniz en az 6 karakter içermelidir.")]
         [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z]).+$", ErrorMessage = "Şifrenizde en az bir büyük ve bir küçük karakter kullanmanız gerekmektedir.")]
-
         public string WalletId { get; set; }
 
     }
--- DigiCash/Services/UserServices.cs
+++ DigiCash/Services/UserServices.cs
@@ -1,11 +1,49 @@
 ﻿using System;
+using DigiCash.Models;
+using Microsoft.AspNetCore.Identity;
 namespace DigiCash.Services
 {
     public class UserServices
     {
-        public UserServices()
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+        private readonly ILogger<UserServices> _logger;
+        private readonly PostgreSqlServices _postgreSqlServices;
+        public UserServices(SignInManager<User> signInManager , UserManager<User> userManager ,
+            ILogger<UserServices> logger , PostgreSqlServices postgreSqlServices)
         {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _logger = logger;
+            _postgreSqlServices = postgreSqlServices;
         }
-    }
+        public async Task<User> CreateUser(String tc , String firstName , String lastName , String password)
+        {
+            User user = new User
+            {
+                firstName = firstName,
+                lastName = lastName,
+                password = password,
+                tc = tc
+            };
+            return user;
+        }
+        public async Task<bool> addUserToDb(User user)
+        {
+            var result = await _userManager.CreateAsync(user);
+            if (result.Succeeded)
+            {
+                Console.WriteLine("oldum");
+                return true;
+            }else
+            {
+                foreach (var error in result.Errors)
+                {
+                    Console.WriteLine(error);
+                }
+                return false;
+            }
+        }
+    }//JWT Generatorler eklenicek
 }
 
